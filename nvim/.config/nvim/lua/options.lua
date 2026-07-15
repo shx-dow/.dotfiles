fbccd6e229d7977b7d3dc7ffa989b281a2c9ed1a
shx-dow <83426772+shx-dow@users.noreply.github.com>
2026-07-16 00:44:49 +0530

    Fix WSL clipboard in tmux and neovim using win32yank

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -3,3 +3,18 @@ require "nvchad.options"
 local o = vim.o
 o.number = true
 o.relativenumber = true
+
+if vim.fn.has "wsl" == 1 then
+  vim.g.clipboard = {
+    name = "WSLClipboard",
+    copy = {
+      ["+"] = "win32yank.exe -i",
+      ["*"] = "win32yank.exe -i",
+    },
+    paste = {
+      ["+"] = "win32yank.exe -o",
+      ["*"] = "win32yank.exe -o",
+    },
+    cache_enabled = 0,
+  }
+end
